35e1c250f2fc7a73dcb66de9bd9d5ceb0385a104
markusFrost <andreywystawkin@yandex.ru>
2016-07-15 15:58:11 +0300

    create login classes

---
+++ AviaSales/app/src/main/java/avia/androi/innopolis/com/aviasales/login/ILoginPresenter.java
@@ -0,0 +1,8 @@
+package avia.androi.innopolis.com.aviasales.login;
+
+import avia.androi.innopolis.com.aviasales.interfaces.IPresenter;
+
+public interface ILoginPresenter extends IPresenter {
+
+    void login();
+}
