cb8bd15e621a9300831ea590da7b314dd399cde9
markusFrost <andreywystawkin@yandex.ru>
2016-07-16 14:26:56 +0300

    create database interface

--- AviaSales/app/src/main/java/avia/androi/innopolis/com/aviasales/history/BookingHistoryLoader.java
+++ AviaSales/app/src/main/java/avia/androi/innopolis/com/aviasales/history/BookingHistoryLoader.java
@@ -4,7 +4,7 @@ import avia.androi.innopolis.com.aviasales.interfaces.ILoader;
 import avia.androi.innopolis.com.aviasales.models.Item;
 
 public class BookingHistoryLoader implements ILoader<Item> {
-    
+
     @Override
     public void load(Item item) {
 
