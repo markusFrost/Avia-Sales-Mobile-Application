2b216cb037b252d877fe08f3140f398ed2e0f2d4
markusFrost <andreywystawkin@yandex.ru>
2016-07-16 14:14:54 +0300

    create search classes

---
+++ AviaSales/app/src/main/java/avia/androi/innopolis/com/aviasales/search/SearchLoader.java
@@ -0,0 +1,11 @@
+package avia.androi.innopolis.com.aviasales.search;
+
+import avia.androi.innopolis.com.aviasales.interfaces.ILoader;
+import avia.androi.innopolis.com.aviasales.models.Item;
+
+public class SearchLoader implements ILoader<Item> {
+    @Override
+    public void load(Item item) {
+        
+    }
+}
---
+++ AviaSales/app/src/main/java/avia/androi/innopolis/com/aviasales/search/SearchPresenter.java
@@ -0,0 +1,23 @@
+package avia.androi.innopolis.com.aviasales.search;
+
+public class SearchPresenter implements ISearchPresenter {
+    @Override
+    public void search() {
+
+    }
+
+    @Override
+    public void onServerSuccess() {
+
+    }
+
+    @Override
+    public void onServerFail() {
+
+    }
+
+    @Override
+    public void onConnectionFail() {
+
+    }
+}
