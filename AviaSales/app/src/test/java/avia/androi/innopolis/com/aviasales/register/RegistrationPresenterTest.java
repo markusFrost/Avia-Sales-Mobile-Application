cbc1efee2edb753e6cd47b1c4a370709998fc9f3
markusFrost <andreywystawkin@yandex.ru>
2016-07-17 14:06:09 +0300

    generate first test

---
+++ AviaSales/app/src/test/java/avia/androi/innopolis/com/aviasales/register/RegistrationPresenterTest.java
@@ -0,0 +1,20 @@
+package avia.androi.innopolis.com.aviasales.register;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RegistrationPresenterTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testRegister() throws Exception {
+
+        assertEquals(4, 2 + 2);
+    }
+}
