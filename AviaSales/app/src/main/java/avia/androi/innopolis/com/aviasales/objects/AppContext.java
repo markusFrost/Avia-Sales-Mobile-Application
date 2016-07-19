0dafb78bfdc4cc3d90b388245c5df0de012b60d6
markusFrost <andreywystawkin@yandex.ru>
2016-07-19 14:36:57 +0300

    create help objects

---
+++ AviaSales/app/src/main/java/avia/androi/innopolis/com/aviasales/objects/AppContext.java
@@ -0,0 +1,11 @@
+package avia.androi.innopolis.com.aviasales.objects;
+
+import android.app.Application;
+
+public class AppContext extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ AviaSales/app/src/main/java/avia/androi/innopolis/com/aviasales/objects/Constants.java
@@ -0,0 +1,5 @@
+package avia.androi.innopolis.com.aviasales.objects;
+
+public class Constants {
+    public static String KEY_USER = "KEY_USER";
+}
